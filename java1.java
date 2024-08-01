7c9d53371940b6834f30b08e79e82aacd0c8ec54
NITESH-DK <nittesh.dk@gmail.com>
2024-08-01 22:51:39 +0530

    day-1-of java-Started

---
+++ java1.java
@@ -0,0 +1,85 @@
+
+//! Boiler Plate --- 
+
+// public class java1{
+// public static void main (String args[]){   
+// }
+
+// }
+
+
+
+
+public class java1 {
+    public static void main (String args[]){
+
+
+//  ! Output  / Print 
+// 1.print 
+// 2. println
+// 3. "\n"
+
+
+  System.out.print("Hello Proffesor");
+//    with the line in the end 
+ System.out.println("Hello Proffesor");
+
+//    with the line in the end 
+ System.out.println("Hello Proffesor \n");
+
+
+
+// ! DATA TYPE 
+
+// ?Byte   , 1Byte = 8bits
+
+//? Primitive
+
+// byte - 1 byte  
+// Range: -128 to 127
+
+// short - 2 bytes
+// Range: -32,768 to 32,767
+
+// char - 2 bytes
+// Range: 0 to 65,535
+
+// boolean - 1 bit (dependent on JVM implementation)
+// Range: true or false
+
+// int - 4 bytes
+// Range: -2^31 to 2^31 -  // -2.147B to 2.147B
+
+// long - 8 bytes
+// Range: -2^63 to 2^63 - // -9.223B to 9.223B
+
+// float - 4 bytes
+// Range: Approximately ±3.40282347E+38F (6-7 significant decimal digits)
+
+// double - 8 bytes
+// Range: Approximately ±1.79769313486231570E+308 (15 significant decimal digits)
+
+
+//  ? Non-Primitive
+// String
+// Array
+// Class
+// Object
+// Interface
+
+byte byt = 8 ;
+short shrt = 122 ;
+char  chr = 'a' ;
+boolean bool = true ;
+int first = 12 ;
+long lng = 1412834676 ;// i think so this is the limit;
+float flt = 13.34f; // be deafult ye double treat hoga so appko 13.34f likhna padega
+double fltr = 13.34 ;
+
+System.out.print(lng) ;
+
+
+
+
+    }
+}
